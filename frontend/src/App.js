b54355d69531d24b5841f1a65183d7a11d2dd3bb
Kaue Felipe Sodate <kauefsodate@gmail.com>
2023-04-03 14:57:07 -0300

    Crud-React-Node

---
+++ frontend/src/App.js
@@ -0,0 +1,39 @@
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
+// Componentes
+import NavBar from './components/layout/NavBar'
+import Footer from './components/layout/Footer'
+import Container from './components/layout/Container'
+import Message from "./components/layout/Message";
+
+
+// Pages
+import Home from './components/pages/Home'
+import Login from './components/pages/auth/Login'
+import Register from './components/pages/auth/Register'
+
+// Context
+import { UsuarioProvider } from "./context/UsuarioContext";
+
+function App() {
+  return (
+    
+      <Router>
+        <UsuarioProvider>
+          <NavBar/>
+          <Message/>
+          <Container>
+            <Routes>
+              <Route path="/" element={<Home/>}/>
+              <Route path="/login" element={<Login/>}/>
+              <Route path="/register" element={<Register/>}/>
+            </Routes>
+          </Container>
+          <Footer/>
+        </UsuarioProvider>
+    </Router>
+    
+  );
+}
+
+export default App;
